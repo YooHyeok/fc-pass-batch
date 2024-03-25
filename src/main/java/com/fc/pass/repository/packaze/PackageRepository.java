e3e09e1cc68415ec2da246fc137eeed33107f3dc
YooHyeok <yjk7454@naver.com>
2024-03-26 00:20:07 +0900

    feat: 서비스에 필요한(회원, 이용권, 패키지, 예약 등) 모든 도메인 추가 (Entity, Enum, Repository)

---
+++ src/main/java/com/fc/pass/repository/packaze/PackageRepository.java
@@ -0,0 +1,14 @@
+package com.fc.pass.repository.packaze;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+}
