8897364ff2ac952301ef7a91f2d29804bc722600
YooHyeok <yjk7454@naver.com>
2024-03-27 00:51:07 +0900

    feat: Pass 도메인 Entity, Enum 수정 및 Repository 추가

--- src/main/java/com/fc/pass/repository/pass/PassEntity.java
+++ src/main/java/com/fc/pass/repository/pass/PassEntity.java
@@ -10,16 +10,14 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "pass")
 public class PassEntity {
-    @Id
-    private int passSeq;
-
-    private String gymId;
-    private String passId;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
+    private Integer passSeq;
+    private Integer packageSeq;
     private String userId;
+
     @Enumerated(EnumType.STRING)
     private PassStatus status;
-    private int num;
-
+    private Integer remainingCount;
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
     private LocalDateTime expiredAt;
@@ -28,5 +26,4 @@ public class PassEntity {
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
-    private boolean deleted;
 }
