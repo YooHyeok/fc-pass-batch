dd16b204cd9f1d70654a0de4dea0617fc549732a
YooHyeok <yjk7454@naver.com>
2024-03-27 00:54:47 +0900

    feat: BatchConfig @EnableBatchProcessing 주석 부연설명 추가

--- src/main/java/com/fc/pass/config/BatchConfig.java
+++ src/main/java/com/fc/pass/config/BatchConfig.java
@@ -3,7 +3,7 @@ package com.fc.pass.config;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing // Batch를 위한 Processing 허용 설정
+@EnableBatchProcessing // Batch를 위한 Processing 허용 설정 - JobBuilderFactory, StepBuilderFactory가 Bean으로 등록된다.
 @Configuration
 public class BatchConfig {
 }
