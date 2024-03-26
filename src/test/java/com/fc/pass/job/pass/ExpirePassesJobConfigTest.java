085123409dfc777468a06bf9e9e42608ac166d83
YooHyeok <yjk7454@naver.com>
2024-03-27 00:55:50 +0900

    faet: 이용권 만료 테스트 클래스 추가 및 테스트로직 구현

---
+++ src/test/java/com/fc/pass/job/pass/ExpirePassesJobConfigTest.java
@@ -0,0 +1,75 @@
+package com.fc.pass.job.pass;
+
+import com.fc.pass.config.TestBatchConfig;
+import com.fc.pass.repository.pass.PassEntity;
+import com.fc.pass.repository.pass.PassRepository;
+import com.fc.pass.repository.pass.PassStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Slf4j
+@SpringBatchTest
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {ExpirePassesJobConfig.class, TestBatchConfig.class})
+public class ExpirePassesJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils; //EndToEnd 테스트를 지원
+    @Autowired
+    private PassRepository passRepository;
+
+    @Test
+    void test_expirePassesStep() throws Exception {
+        //given
+        addPassEntities(10);
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        JobInstance jobInstance = jobExecution.getJobInstance();
+
+        //then
+        assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+        assertEquals("expirePassesJob", jobInstance.getJobName());
+    }
+
+    /**
+     * Pass 테이블에 데이터가 없으므로 기본적인 랜덤한 값을 넣어주는 작업을 수행한다.
+     */
+    private void addPassEntities(int size) {
+        final LocalDateTime now = LocalDateTime.now();
+        final Random random = new Random();
+
+        List<PassEntity> passEntities = new ArrayList<>();
+        for (int i = 0; i <size ; i++) {
+            PassEntity passEntity = new PassEntity();
+            passEntity.setPackageSeq(1);
+            passEntity.setUserId("A" + 1000000 + i);
+            passEntity.setStatus(PassStatus.PROGRESSED);
+            passEntity.setRemainingCount(random.nextInt(11));
+            passEntity.setStartedAt(now.minusDays(60));
+            passEntity.setEndedAt(now.minusDays(1));
+            passEntities.add(passEntity);
+        }
+        passRepository.saveAll(passEntities);
+
+    }
+}
