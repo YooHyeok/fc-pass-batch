a07fc6a7b13c7af529c850ad2d08cdf9da23bb06
YooHyeok <yjk7454@naver.com>
2024-03-31 00:07:30 +0900

    feat: Pass 도메인 수정 - BaseEntity상속 및 Repository 이용권 잔여횟수 차감 update JPQL 구현

--- src/main/java/com/fc/pass/repository/pass/PassEntity.java
+++ src/main/java/com/fc/pass/repository/pass/PassEntity.java
@@ -1,5 +1,6 @@
 package com.fc.pass.repository.pass;
 
+import com.fc.pass.repository.BaseEntity;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -9,7 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @Entity
 @Table(name = "pass")
-public class PassEntity {
+public class PassEntity extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
     private Integer passSeq;
     private Integer packageSeq;
@@ -21,9 +22,5 @@ public class PassEntity {
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
     private LocalDateTime expiredAt;
-
-    @CreationTimestamp
-    @Column(updatable = false, nullable = false)
-    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/fc/pass/repository/pass/PassRepository.java
+++ src/main/java/com/fc/pass/repository/pass/PassRepository.java
@@ -1,7 +1,26 @@
 package com.fc.pass.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
 
+    /**
+     * [수업 종료 후 이용권 차감] <br/>
+     * 예약된 수업의 이용권의 잔여 횟수를 수정한다.
+     * @param passSeq
+     * @param remainingCount
+     * @return
+     */
+    @Transactional
+    @Modifying
+    @Query(value = """
+        UPDATE PassEntity p
+           SET p.remainingCount = :remainingCount,
+               p.modifiedAt = CURRENT_TIMESTAMP 
+         WHERE p.passSeq = :passSeq
+         """)
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
 }
