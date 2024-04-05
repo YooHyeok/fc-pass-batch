e7b7254e53ad2995810a8b9118beb3a11cd0db51
YooHyeok <yjk7454@naver.com>
2024-04-05 09:49:20 +0900

    fix: Batch 초기 애플리케이션 실행 생성 테스트용 Sample코드 주석처리

--- src/main/java/com/fc/pass/PassBatchApplication.java
+++ src/main/java/com/fc/pass/PassBatchApplication.java
@@ -20,39 +20,39 @@ public class PassBatchApplication {
 	/**
 	 * Job을 만들기 위해서는 Step을 만든 후 Step을 기반으로 Job을 구성한다.
 	 */
-	private final JobBuilderFactory jobBuilderFactory;
-	private final StepBuilderFactory stepBuilderFactory;
-
-	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-		this.jobBuilderFactory = jobBuilderFactory;
-		this.stepBuilderFactory = stepBuilderFactory;
-	}
+//	private final JobBuilderFactory jobBuilderFactory;
+//	private final StepBuilderFactory stepBuilderFactory;
+//
+//	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+//		this.jobBuilderFactory = jobBuilderFactory;
+//		this.stepBuilderFactory = stepBuilderFactory;
+//	}
 
 	/**
 	 * Step 선언
 	 * @return
 	 */
-	@Bean
-	public Step passStep() {
-		return this.stepBuilderFactory.get("passStep") // Step의 이름을 선언한다.
-				.tasklet((contribution, chunkContext) -> { //Tasklet 방식으로 Tasklet 인터페이스의 execute() 메소드를 람다로 구현
-					System.out.println("Execute PassStep");
-					System.out.println("contribution = " + contribution + ", chunkContext = " + chunkContext);
-					return RepeatStatus.FINISHED; // 종료
-				}).build();
-	}
+//	@Bean
+//	public Step passStep() {
+//		return this.stepBuilderFactory.get("passStep") // Step의 이름을 선언한다.
+//				.tasklet((contribution, chunkContext) -> { //Tasklet 방식으로 Tasklet 인터페이스의 execute() 메소드를 람다로 구현
+//					System.out.println("Execute PassStep");
+//					System.out.println("contribution = " + contribution + ", chunkContext = " + chunkContext);
+//					return RepeatStatus.FINISHED; // 종료
+//				}).build();
+//	}
 
 	/**
 	 * Job 선언
 	 * passStep을 구성해준다.
 	 * @return
 	 */
-	@Bean
-	public Job passJob() {
-		return this.jobBuilderFactory.get("passJob")
-				.start(passStep())
-				.build();
-	}
+//	@Bean
+//	public Job passJob() {
+//		return this.jobBuilderFactory.get("passJob")
+//				.start(passStep())
+//				.build();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
