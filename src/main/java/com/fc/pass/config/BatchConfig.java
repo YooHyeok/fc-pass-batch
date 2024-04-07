65a199a6436617f8bbdd491b6ff85bdaaaa95ef2
webdevyoo <yjk7454@naver.com>
2024-04-07 20:40:24 +0900

    feat: BatchConfig - JobRegistry 활성화 JobRegistryBeanPostProcessor 빈 등록

--- src/main/java/com/fc/pass/config/BatchConfig.java
+++ src/main/java/com/fc/pass/config/BatchConfig.java
@@ -1,9 +1,25 @@
 package com.fc.pass.config;
 
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableBatchProcessing // Batch를 위한 Processing 허용 설정 - JobBuilderFactory, StepBuilderFactory가 Bean으로 등록된다.
 @Configuration
 public class BatchConfig {
+    /**
+     * JobRegistry는 context에서 Job을 추적할 때 유용하다.
+     * JobRegistryProcessor는 Application Context가 올라가면서 bean등록시, 자동으로 JobRegistry에 Job을 등록시켜준다.
+     *
+     * @param jobRegistry
+     * @return
+     */
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
+    }
 }
